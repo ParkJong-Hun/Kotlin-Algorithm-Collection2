b7a7718da0aa0dce94ae2917eec9bcb796c26c54
ParkJong-Hun <thesn515@gmail.com>
2022-05-25 20:45:44 +0900

    algo:0~9の数の中で入ってない数の和

--- src/algorithms/NotContainingNumber.kt
+++ src/algorithms/NotContainingNumber.kt
@@ -1,8 +1,13 @@
 package algorithms
 
+import template.Info
+
 //0~9の数の中で入ってない数の和
 object NotContainingNumber {
     operator fun invoke(numbers: List<Int>): Int {
-        return (0 .. 9).filter { !numbers.contains(it) }.reduce{result, number -> result + number}
+        return (0 .. 9).filter { !numbers.contains(it) }.reduce{ result, number ->
+            Info(number)
+            return@reduce result + number
+        }
     }
 }
